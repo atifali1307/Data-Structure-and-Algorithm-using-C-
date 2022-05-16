80d37f35887d7470457bad0f45d475f1082f0220
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-05-16 14:51:15 +0530

    Add files via upload

---
+++ SORT_FUN.C
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  void bubblesort(int [],int,int);
+  void inputarray(int [],int,int);
+  void outputarray(int [],int,int);
+  int a[10],i,n,ch;
+  clrscr();
+  while(1)
+  {
+  printf("\n\n1. Input Array Elements\n");
+  printf("2. Output Array Elements\n");
+  printf("3. Selection Sort\n");
+  printf("4. Bubble Sort\n");
+  printf("5. Exit\n\n");
+  printf("Enter your choice: ");
+  scanf("%d",&ch);
+  switch(ch)
+  {
+    case 1:
+	 printf("Enter Number of Elements of an Array: \n");
+	 scanf("%d",&n);
+	 inputarray(a,0,n);
+	 break;
+    case 2:
+	 outputarray(a,0,n);
+	 break;
+    case 4:
+	  bubblesort(a,0,n);
+    case 5:
+	  exit(1);
+  }
+  }
+  getch();
+}
+void inputarray(int a[],int l,int r)
+{
+  int i;
+  for(i=l;i<r;i++)
+  {
+    printf("a[%d]: ",i);
+    scanf("%d",&a[i]);
+  }
+}
+void outputarray(int a[],int l,int r)
+{
+  int i;
+  printf("Array Elements are: \n\n");
+  for(i=l;i<r;i++)
+  {
+    printf("%d ",a[i]);
+  }
+}
+void bubblesort(int a[],int l,int r)
+{
+  int pass,temp,j,flag=1,n=r;
+  printf("\n\nSorted Array is:\n");
+  for(pass=l;pass<r && flag;pass++)
+  {
+    flag=0;
+    for(j=l;j<r-1-pass;j++)
+    {
+      if(a[j]>a[j+1])
+      {
+	flag=1;
+	temp=a[j];
+	a[j]=a[j+1];
+	a[j+1]=temp;
+      }
+    }
+  }
+  outputarray(a,0,n);
+}
