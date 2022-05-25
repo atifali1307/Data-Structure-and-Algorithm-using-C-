53a6d143d1241dcb01296500cb93290fc827d148
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-05-25 21:06:23 +0530

    Add files via upload

--- HEAPSORT.C
+++ HEAPSORT.C
@@ -1,54 +1,72 @@
 #include<stdio.h>
 #include<conio.h>
+void heap_sort(int [],int);
+void build_max_heap(int [],int);
+void max_heapify(int [],int,int);
 void main()
 {
-  void maxheapify(int [],int,int);
-  void heapsort(int [],int);
-  void printarr(int [],int);
-  int a[]={48,10,23,43,28,26,1};
-  int n=length(a);
-  printf("Before Sorting Array Elements are: \n\n");
-  printarr(a,n);
-  heapsort(a,n);
-  printf("\n\nAfter sorting Array Elements are: \n\n");
-  printarr(a,n);
-  getch();
-}
-void printarr(int arr[],int n)
-{
-  int i;
+  int i,r,heapsize,n;
+  int a[50];
+  clrscr();
+  printf("How many Elements you want to insert: ");
+  scanf("%d",&n);
   for(i=0;i<n;i++)
   {
-	printf("%d ",arr[i]);
+    printf("a[%d]= ",i);
+    scanf("%d",&a[i]);
+  }
+  heapsize=n-1;
+  heap_sort(a,heapsize);
+  printf("Sorted Elements are: ");
+  for(i=0;i<n;i++)
+  {
+    printf("%d ",a[i]);
+  }
+  getch();
+}
+void heap_sort(int a[],int heapsize)
+{
+  int i,temp;
+  build_max_heap(a,heapsize);
+  for(i=heapsize;i>0;i--)
+  {
+    temp=a[i];
+    a[i]=a[0];
+    a[0]=temp;
+    heapsize--;
+    max_heapify(a,0,heapsize);
   }
 }
-void heapsort(int a[],int n)
+void build_max_heap(int a[],int heapsize)
 {
   int i;
-  for(i=n/2-1;i>=0;i--)
-	heap1(a,n,i);
-  for(i=n-1;i>=0;i--)
+  for(i=heapsize/2;i>=0;i--)
   {
-	int temp=a[0];
-	a[0]=a[i];
-	a[i]=temp;
-	maxheapify(a,i,0);
+    max_heapify(a,i,heapsize);
   }
 }
-void maxheapify(int a[],int n,int i)
+void max_heapify(int a[],int i,int heapsize)
 {
-  int largest=i;
-  int left=2*i+1;
-  int right=2*i+2;
-  if(left<n && a[left]>a[largest])
-	largest=left;
-  if(right<n && a[right]>a[largest])
-	largest=right;
+  int temp,largest;
+  int l=(2*i)+1;
+  int r=(2*i)+2;
+  if((l<=heapsize)&&(a[l]>a[i]))
+  {
+    largest=l;
+  }
+  else
+  {
+    largest=i;
+  }
+  if((r<=heapsize)&&(a[r]>a[largest]))
+  {
+    largest=r;
+  }
   if(largest!=i)
   {
-	int temp=a[i];
-	a[i]=a[largest];
-	a[largest]=temp;
-	maxheapify(a,n,largest);
+    temp=a[i];
+    a[i]=a[largest];
+    a[largest]=temp;
+    max_heapify(a,largest,heapsize);
   }
 }
